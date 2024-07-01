83ab7209749c2328a63b8a87337e73a4d4a6d4db
gitshashikiran <GladyceGoyette2000@outlook.com>
2024-07-01 12:29:10 +0530

    Update terraformstatefile.tf

--- terraformstatefile.tf
+++ terraformstatefile.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "azurerm" {
 resource_group_name = "shashirg"
 storage_account_name = "shashistg"
 container_name = "shashictr"
 key = "prod.terraform.tfstate"
   }
-}*/
+}
